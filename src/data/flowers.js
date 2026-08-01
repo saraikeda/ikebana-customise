d2d3b0925a17e2f3c512a5a0cdbdd2075912a937
saraiked <saraiked@andrew.cmu.edu>
2026-07-31 20:50:03 -0400

    added images for flowers

--- src/data/flowers.js
+++ src/data/flowers.js
@@ -3,44 +3,27 @@
 export const flowers = [
   {
     id: 1,
-    name: "Placeholder Flower 1",
-    image:
-      "data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='80' height='160' viewBox='0 0 80 160'%3E%3Crect x='36' y='20' width='8' height='120' rx='4' fill='%236b7280'/%3E%3C/svg%3E",
+    name: "Plum Flower (Ume)",
+    image: "../images/ume.PNG"  
   },
   {
     id: 2,
-    name: "Placeholder Flower 2",
-    image:
-      "data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='80' height='160' viewBox='0 0 80 160'%3E%3Crect x='36' y='16' width='8' height='124' rx='4' fill='%234b5563'/%3E%3C/svg%3E",
+    name: "Cherry Blossom (Sakura)",
+    image: "../images/sakura.PNG"  
   },
   {
     id: 3,
-    name: "Placeholder Flower 3",
-    image:
-      "data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='80' height='160' viewBox='0 0 80 160'%3E%3Crect x='36' y='24' width='8' height='116' rx='4' fill='%236b7280'/%3E%3C/svg%3E",
+    name: "Chrysanthemum (Kiku)",
+    image: "../images/kiku.PNG"  
   },
   {
     id: 4,
-    name: "Placeholder Flower 4",
-    image:
-      "data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='80' height='160' viewBox='0 0 80 160'%3E%3Crect x='36' y='18' width='8' height='122' rx='4' fill='%234b5563'/%3E%3C/svg%3E",
+    name: "Camellia (Tsubaki)",
+    image: "../images/tsubaki.PNG"  
   },
   {
     id: 5,
-    name: "Placeholder Flower 5",
-    image:
-      "data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='80' height='160' viewBox='0 0 80 160'%3E%3Crect x='36' y='22' width='8' height='118' rx='4' fill='%236b7280'/%3E%3C/svg%3E",
-  },
-  {
-    id: 6,
-    name: "Placeholder Flower 6",
-    image:
-      "data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='80' height='160' viewBox='0 0 80 160'%3E%3Crect x='36' y='14' width='8' height='126' rx='4' fill='%234b5563'/%3E%3C/svg%3E",
-  },
-  {
-    id: 7,
-    name: "Placeholder Flower 7",
-    image:
-      "data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='80' height='160' viewBox='0 0 80 160'%3E%3Crect x='36' y='20' width='8' height='120' rx='4' fill='%236b7280'/%3E%3C/svg%3E",
+    name: "Pine (Matsu)",
+    image: "../images/pines.PNG"  
   },
 ];
